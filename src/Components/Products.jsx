0886d0fa7c14574e446cd9dea33fdc24a43cad08
Drew Kullman <agkullman@protonmail.com>
2023-12-15 07:10:41 -0900

    Commit 1: Solved

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,10 +1,17 @@
-const Products = () => {
+const Products = ({products}) => {
     // Instructions:
     //     1. Import the products state as props
     //     2. Loop over useing #map 
     //     3. Show the product name in an h1, and the product price in a p tag
+console.log({products})
+return  products.map((product) => (
+            <div className='product' key={product.id}>
+                <h1>{product.productData.name}</h1>
+                <p>{product.productData.pricing}</p> {/* Assuming it's 'price' not 'pricing' */}
+            </div>
+        ))
 
-    return null
+
 }
 
 export default Products
